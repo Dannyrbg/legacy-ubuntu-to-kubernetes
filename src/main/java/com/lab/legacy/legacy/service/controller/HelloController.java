a5df51b7e6ed116e6c3aabf75711c58212a73f77
Daniel Gruen <d.ray.gruen@gmail.com>
2026-02-13 16:53:43 -0500

    Squashed 'apps/legacy-service/' changes from a25a656..79f9b3b
    79f9b3b Changed Controller HTTP endpoint to output JSON
    
    git-subtree-dir: apps/legacy-service
    git-subtree-split: 79f9b3bf9740d576673584fbec125c99845f9f1b

--- src/main/java/com/lab/legacy/legacy/service/controller/HelloController.java
+++ src/main/java/com/lab/legacy/legacy/service/controller/HelloController.java
@@ -2,12 +2,16 @@ package com.lab.legacy.legacy.service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
 	@GetMapping("/hello")
-	public String hello() {
-		return "Hello from legacy Ubuntu\n";
+	public Map<String, String> hello() {
+		return Map.of(
+			"status", "ok",
+			"service", "legacy-service"
+		);
 	}
 }
